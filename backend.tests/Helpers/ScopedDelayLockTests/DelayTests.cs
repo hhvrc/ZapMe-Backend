b272ea3a2c2b3d28df650103bce69c687d14489a
hhvrc <github@heavenvr.tech>
2022-11-27 18:26:59 +0100

    Add test case for ScopedDelayLock

---
+++ backend.tests/Helpers/ScopedDelayLockTests/DelayTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using ZapMe.Helpers;
+
+namespace ZapMe.Tests.Helpers.ScopedDelayLockTests;
+
+public sealed class DelayTests
+{
+    [Fact]
+    public async void DelayHundredMilliseconds()
+    {
+        Stopwatch sw = new Stopwatch();
+
+        sw.Start();
+        {
+            await using ScopedDelayLock sdl = new ScopedDelayLock(TimeSpan.FromMilliseconds(100));
+        }
+        sw.Stop();
+
+        Assert.InRange(sw.ElapsedMilliseconds, 95, 105);
+    }
+}
