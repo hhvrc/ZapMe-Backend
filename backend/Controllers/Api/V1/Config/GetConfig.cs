500c6673916387989f9e08812b0584d28530021d
hhvrc <github@heavenvr.tech>
2023-01-18 01:02:50 +0100

    some changes

--- backend/Controllers/Api/V1/Config/GetConfig.cs
+++ backend/Controllers/Api/V1/Config/GetConfig.cs
@@ -9,19 +9,15 @@ public partial class ConfigController
     /// <summary>
     /// 
     /// </summary>
-    /// <param name="cancellationToken"></param>
-    /// <returns>The user account</returns>
-    /// <response code="200">Returns users account</response>
-    /// <response code="500">Error details</response>
+    /// <returns>The config for the service</returns>
+    /// <response code="200">Returns the service config</response>
     [HttpGet(Name = "GetConfig")]
     [Produces(Application.Json, Application.Xml)]
     [ProducesResponseType(typeof(Config.Models.Config), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ErrorDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(typeof(ErrorDetails), StatusCodes.Status500InternalServerError)]
-    public async Task<IActionResult> GetConfig(CancellationToken cancellationToken)
+    public Config.Models.Config GetConfig()
     {
-        await Task.CompletedTask;
-
-        return Ok(new Config.Models.Config());
+        return new Config.Models.Config();
     }
 }
