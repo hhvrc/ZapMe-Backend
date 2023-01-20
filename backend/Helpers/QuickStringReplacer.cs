140da01f3ea693ee49fc498665b000db2176e3cd
hhvrc <github@heavenvr.tech>
2023-01-20 15:21:13 +0100

    Optimize and clean up QuickStringReplacer

--- backend/Helpers/QuickStringReplacer.cs
+++ backend/Helpers/QuickStringReplacer.cs
@@ -1,91 +1,83 @@
-﻿namespace ZapMe.Helpers;
+﻿using System.Runtime.InteropServices;
+
+namespace ZapMe.Helpers;
 
 public sealed class QuickStringReplacer
 {
-    private readonly struct ReplaceAction
+    private readonly record struct Replacement(int MatchStart, int MatchEnd, string New);
+
+    private readonly string _source;
+    private readonly List<Replacement> _replacements = new(8);
+    private int _diff;
+
+    public QuickStringReplacer(string source)
     {
-        public int MatchStart { get; init; }
-        public int MatchEnd { get; init; }
-        public string Replacement { get; init; }
+        _source = source;
     }
 
-    private readonly string _value;
-    private readonly List<ReplaceAction> _replacements;
-    private int _sizeDiff;
-
-    public QuickStringReplacer(string value)
+    public QuickStringReplacer Replace(ReadOnlySpan<char> oldValue, string newValue)
     {
-        _value = value;
-        _replacements = new List<ReplaceAction>();
-        _sizeDiff = 0;
-    }
+        ArgumentNullException.ThrowIfNull(newValue);
 
-    public QuickStringReplacer Replace(ReadOnlySpan<char> match, string replacement)
-    {
-        int matchLength = match.Length;
-        if (matchLength > 0)
+        int oldLength = oldValue.Length;
+        if (oldLength <= 0)
         {
-            ReadOnlySpan<char> value = _value.AsSpan();
-            int sizeDiff = replacement.Length - matchLength;
+            return this;
+        }
 
-            int relativeIndex;
-            int absolutePosition = 0;
+        ReadOnlySpan<char> source = _source.AsSpan();
+        int diff = newValue.Length - oldLength;
+        int abs = 0, rel;
 
-            while ((relativeIndex = value.IndexOf(match)) != -1)
-            {
-                int absoluteStart = absolutePosition + relativeIndex;
-                int relativeEnd = relativeIndex + matchLength;
+        while ((rel = source[abs..].IndexOf(oldValue)) != -1)
+        {
+            int pos = abs + rel;
 
-                _replacements.Add(new ReplaceAction
-                {
-                    MatchStart = absoluteStart,
-                    MatchEnd = absoluteStart + matchLength,
-                    Replacement = replacement
-                });
-                _sizeDiff += sizeDiff;
-
-                value = value[relativeEnd..];
-                absolutePosition += relativeEnd;
-            }
+            _replacements.Add(new(pos, abs = pos + oldLength, newValue));
+            _diff += diff;
         }
 
         return this;
     }
 
+    private static int Compare(Replacement left, Replacement right) => left.MatchStart - right.MatchStart;
+    private static readonly Comparison<Replacement> _Comparison = new(Compare);
+
     public override string ToString()
     {
-        int newStringSize = _value.Length + _sizeDiff;
-        _replacements.Sort(static (x, y) => x.MatchStart - y.MatchStart);
+        int newLength = _source.Length + _diff;
 
-        return String.Create(newStringSize, this, static (span, qsr) =>
+        return String.Create(newLength, this, static (span, state) =>
         {
-            int resizedSize = 0;
-            int valueEndIndex = 0;
-            ReadOnlySpan<char> value = qsr._value;
+            ReadOnlySpan<char> source = state._source.AsSpan();
+            Span<Replacement> replacements = CollectionsMarshal.AsSpan(state._replacements);
+            replacements.Sort(_Comparison);
 
-            foreach (ReplaceAction action in qsr._replacements)
+            int offset = 0, prev = 0;
+
+            foreach (Replacement r in replacements)
             {
-                int originalChunkLength = action.MatchStart - valueEndIndex;
-                if (originalChunkLength > 0)
+                int chunk = r.MatchStart - prev;
+                if (chunk > 0)
                 {
-                    value.Slice(valueEndIndex, originalChunkLength).CopyTo(span[resizedSize..]);
-                    resizedSize += originalChunkLength;
+                    source.Slice(prev, chunk).CopyTo(span[offset..]);
+                    offset += chunk;
                 }
 
-                ReadOnlySpan<char> replacement = action.Replacement.AsSpan();
-                if (!replacement.IsEmpty)
+                ReadOnlySpan<char> rNew = r.New.AsSpan();
+                if (!rNew.IsEmpty)
                 {
-                    replacement.CopyTo(span[resizedSize..]);
-                    resizedSize += replacement.Length;
+                    rNew.CopyTo(span[offset..]);
+                    offset += rNew.Length;
                 }
 
-                valueEndIndex = action.MatchEnd;
+                prev = r.MatchEnd;
             }
 
-            int remainingLength = value.Length - valueEndIndex;
-            if (remainingLength > 0)
+            int remainder = source.Length - prev;
+            if (remainder > 0)
             {
-                value.Slice(valueEndIndex, remainingLength).CopyTo(span[resizedSize..]);
+                source[prev..].CopyTo(span[offset..]);
             }
         });
     }
