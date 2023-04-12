27eaa695d219f92eef60a59587cb5585e5d45332
hhvrc <github@heavenvr.tech>
2023-04-12 14:18:52 +0200

    Simplify Generator test

--- backend.tests/Utils/StringUtilsTests.cs
+++ backend.tests/Utils/StringUtilsTests.cs
@@ -7,9 +7,6 @@ public sealed class UtilTests
     [Fact]
     public void GenerateRandomString_Length()
     {
-        for (int i = 0; i < 1000; i++)
-        {
-            Assert.Equal(32, StringUtils.GenerateUrlSafeRandomString(32).Length);
-        }
+        Assert.Equal(32, StringUtils.GenerateUrlSafeRandomString(32).Length);
     }
 }
