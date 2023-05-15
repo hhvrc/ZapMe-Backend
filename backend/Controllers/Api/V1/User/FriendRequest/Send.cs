8209a31013826e4ed46965ee97d7a218b5b0107a
hhvrc <113805205+hhvrc@users.noreply.github.com>
2023-05-15 19:15:59 +0200

    Implement SendFriendRequest

--- backend/Controllers/Api/V1/User/FriendRequest/Send.cs
+++ backend/Controllers/Api/V1/User/FriendRequest/Send.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ZapMe.Authentication;
+using ZapMe.Data.Models;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace ZapMe.Controllers.Api.V1;
@@ -9,6 +12,7 @@ partial class UserController
     /// Send friend request
     /// </summary>
     /// <param name="userId"></param>
+    /// <param name="cancellationToken"></param>
     /// <returns></returns>
     /// <response code="200"></response>
     /// <response code="404"></response>
@@ -18,8 +22,36 @@ partial class UserController
     [Produces(Application.Json)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public IActionResult FriendRequestSend([FromRoute] Guid userId)
+    public async Task<IActionResult> FriendRequestSend([FromRoute] Guid userId, CancellationToken cancellationToken)
     {
+        UserEntity user = (User as ZapMePrincipal)!.Identity.User;
+
+        // Check if requesting user has blocked target user, or if user is trying to send a friend request to themselves
+        if (user.Relations!.Any(u => u.TargetUserId == userId && u.RelationType == UserRelationType.Blocked) || user.Id == userId)
+        {
+            return BadRequest();
+        }
+
+        // Get target user if exists and has not blocked the requesting user
+        UserEntity? targetUser = await _dbContext.Users.Where(u => u.Id == userId && !u.Relations!.Any(r => r.TargetUserId == user.Id && r.RelationType == UserRelationType.Blocked)).Include(u => u.Relations).FirstOrDefaultAsync(cancellationToken);
+        if (targetUser == null)
+        {
+            return NotFound();
+        }
+
+        // Check if target user has already sent a friend request
+        if (await _dbContext.FriendRequests.AnyAsync(r => r.SenderId == userId && r.ReceiverId == user.Id, cancellationToken))
+        {
+            return NotFound();
+        }
+
+        await _dbContext.FriendRequests.AddAsync(new()
+        {
+            SenderId = (User.Identity as ZapMeIdentity)!.UserId,
+            ReceiverId = userId
+        }, cancellationToken);
+        await _dbContext.SaveChangesAsync(cancellationToken);
+
         return Ok(userId);
     }
 }
