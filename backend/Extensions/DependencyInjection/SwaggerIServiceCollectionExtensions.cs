180d2ce7dd0e6449b8e5e6dab306b67dcafd62e0
hhvrc <github@heavenvr.tech>
2023-05-31 10:29:42 +0200

    Add Auth to swagger

--- backend/Extensions/DependencyInjection/SwaggerIServiceCollectionExtensions.cs
+++ backend/Extensions/DependencyInjection/SwaggerIServiceCollectionExtensions.cs
@@ -36,6 +36,37 @@ public static class SwaggerIServiceCollectionExtensions
                     Url = new Uri(App.LicenseUrl)
                 },
             });
+            opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = @"JWT Authorization header using the Bearer scheme.
+
+
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      
+
+                        Example: 'Bearer 12345abcdef'",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Id = "Bearer",
+                            Type = ReferenceType.SecurityScheme
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    Array.Empty<string>()
+                }
+            });
             if (isDev)
             {
                 opt.AddServer(new OpenApiServer { Url = "http://localhost:5296" });
