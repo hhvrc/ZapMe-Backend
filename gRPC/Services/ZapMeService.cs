ad8837af74f9ad148ef60179cf8e5b19a725d340
hhvrc <github@heavenvr.tech>
2023-06-12 13:31:46 +0200

    Add more gRPC types

--- gRPC/Services/ZapMeService.cs
+++ gRPC/Services/ZapMeService.cs
@@ -1,6 +1,4 @@
-﻿using Grpc.Core;
-
-namespace ZapMe.gRPC.Services;
+﻿namespace ZapMe.gRPC.Services;
 
 public class ZapMeGrpcServiceImpl : ZapMeGrpcService.ZapMeGrpcServiceBase
 {
@@ -14,21 +12,5 @@ public class ZapMeGrpcServiceImpl : ZapMeGrpcService.ZapMeGrpcServiceBase
     public Task Initialize()
     {
         return Task.CompletedTask;
-    }
-
-    public override Task<Empty> Ping(Empty request, ServerCallContext context)
-    {
-        return Task.FromResult(new Empty());
-    }
-
-    public override Task<WebRtcConnectReply> WebRtcConnect(WebRtcConnectRequest request, ServerCallContext context)
-    {
-        _logger.LogInformation("WebRtcConnect");
-
-        return Task.FromResult(new WebRtcConnectReply
-        {
-            SentOffer = true,
-            RequestExpiresAt = DateTimeOffset.UtcNow.AddSeconds(30).ToUnixTimeSeconds()
-        });
     }
 }
