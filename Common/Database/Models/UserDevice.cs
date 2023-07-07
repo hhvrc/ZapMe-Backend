420660235de698a73f1bf855e6345c6a4819d376
hhvrc <github@heavenvr.tech>
2023-07-07 18:50:00 +0200

    Add accesstoken to UserDevice

--- Common/Database/Models/UserDevice.cs
+++ Common/Database/Models/UserDevice.cs
@@ -12,6 +12,11 @@ public sealed class UserDeviceEntity
 
     public required string Name { get; set; }
 
+    /// <summary>
+    /// Access key for the device. Used to authenticate the device with the WebSocket endpoint.
+    /// </summary>
+    public required string AccessToken { get; set; }
+
     public Guid? IconId { get; set; }
 
     public DateTime CreatedAt { get; private set; }
